ef60d5881c0ef372db8d48fd604ea48502fae1be
andrewallenbruce <andrewbruce.himni@gmail.com>
2022-10-14 20:11:18 -0400

    tests

--- tests/testthat/test-provider_luhn.R
+++ tests/testthat/test-provider_luhn.R
@@ -1,3 +1,6 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("`provider_luhn()` works", {
+  expect_equal(provider_luhn(npi = 1528060837), TRUE)
+  expect_equal(provider_luhn(npi = "1528060837"), TRUE)
+  expect_equal(provider_luhn(npi = 1234567891), FALSE)
+  expect_equal(provider_luhn(npi = "abcdefghij"), FALSE)
 })
