93d9652354b673f411fe1cb9c5ca522b2ebb9fb8
andrewallenbruce <andrewbruce.himni@gmail.com>
2022-10-16 22:11:15 -0400

    tests

--- tests/testthat/test-format_zip.R
+++ tests/testthat/test-format_zip.R
@@ -1,3 +1,6 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("`age_days()` works", {
+  zip_ex <- "12345-6789"
+  zip_ex2 <- "12345"
+  expect_equal(format_zipcode(123456789), zip_ex)
+  expect_equal(format_zipcode(12345), zip_ex2)
 })
