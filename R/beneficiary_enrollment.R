783c648d4f1397dbc94d9feb2b9fd54b3193028f
Andrew Bruce <andrewbruce.himni@gmail.com>
2023-05-30 03:00:06 -0400

    commit

--- R/beneficiary_enrollment.R
+++ R/beneficiary_enrollment.R
@@ -66,21 +66,27 @@ beneficiary_enrollment <- function(year        = NULL,
                                    tidy        = TRUE) {
 
   # match args ----------------------------------------------------
-  if (!is.null(period)) {rlang::arg_match0(period, c("Year", month.name))}
+  if (!is.null(period)) {rlang::arg_match0(period, c("Year", "Monthly", month.name))}
   if (!is.null(level)) {rlang::arg_match0(level, c("National", "State", "County"))}
   if (!is.null(state)) {rlang::arg_match0(state, c(state.abb))}
   if (!is.null(state_name)) {rlang::arg_match0(state_name, c(state.name))}
 
   # args tribble ------------------------------------------------------------
   args <- tibble::tribble(
-                     ~x,        ~y,
-                 "YEAR",      year,
+                     ~x,         ~y,
+                 "YEAR",       year,
                 "MONTH",     period,
-         "BENE_GEO_LVL",     level,
-    "BENE_STATE_ABRVTN",     state,
-      "BENE_STATE_DESC",state_name,
-     "BENE_COUNTY_DESC",    county,
-         "BENE_FIPS_CD",      fips)
+         "BENE_GEO_LVL",      level,
+    "BENE_STATE_ABRVTN",      state,
+      "BENE_STATE_DESC", state_name,
+     "BENE_COUNTY_DESC",     county,
+         "BENE_FIPS_CD",       fips)
+
+  if (period == "Monthly") {
+    args <- args |>
+      tidyr::unnest(y) |>
+      dplyr::filter(x != "MONTH")
+  }
 
   # map param_format and collapse -------------------------------------------
   params_args <- purrr::map2(args$x, args$y, param_format) |>
@@ -99,7 +105,7 @@ beneficiary_enrollment <- function(year        = NULL,
   response <- httr2::request(url) |> httr2::req_perform()
 
   # no search results returns empty tibble ----------------------------------
-  if (httr2::resp_header(response, "content-length") == "0") {
+  if (as.integer(httr2::resp_header(response, "content-length")) <= 28L) {
 
     cli_args <- tibble::tribble(
       ~x,           ~y,
@@ -131,31 +137,31 @@ beneficiary_enrollment <- function(year        = NULL,
   if (tidy) {
 
     results <- dplyr::rename_with(results, str_to_snakecase) |>
-    dplyr::rename(level                       = bene_geo_lvl,
-                  period                      = month,
-                  state                       = bene_state_abrvtn,
-                  state_name                  = bene_state_desc,
-                  county                      = bene_county_desc,
-                  fips                        = bene_fips_cd,
-                  bene_total                  = tot_benes,
-                  bene_orig                   = orgnl_mdcr_benes,
-                  bene_ma_oth                 = ma_and_oth_benes,
-                  bene_aged_total             = aged_tot_benes,
-                  bene_aged_esrd              = aged_esrd_benes,
-                  bene_aged_no_esrd           = aged_no_esrd_benes,
-                  bene_dsb_total              = dsbld_tot_benes,
-                  bene_dsb_esrd      = dsbld_esrd_and_esrd_only_benes,
-                  bene_dsb_no_esrd            = dsbld_no_esrd_benes,
-                  bene_ab_total               = a_b_tot_benes,
-                  bene_ab_orig                = a_b_orgnl_mdcr_benes,
-                  bene_ab_ma_oth              = a_b_ma_and_oth_benes,
-                  bene_rx_total               = prscrptn_drug_tot_benes,
-                  bene_rx_pdp                 = prscrptn_drug_pdp_benes,
-                  bene_rx_mapd                = prscrptn_drug_mapd_benes,
-                  bene_rx_elig = prscrptn_drug_deemed_eligible_full_lis_benes,
-                  bene_rx_full = prscrptn_drug_full_lis_benes,
-                  bene_rx_part = prscrptn_drug_partial_lis_benes,
-                  bene_rx_none = prscrptn_drug_no_lis_benes) |>
+    dplyr::rename(level             = bene_geo_lvl,
+                  period            = month,
+                  state             = bene_state_abrvtn,
+                  state_name        = bene_state_desc,
+                  county            = bene_county_desc,
+                  fips              = bene_fips_cd,
+                  bene_total        = tot_benes,
+                  bene_orig         = orgnl_mdcr_benes,
+                  bene_ma_oth       = ma_and_oth_benes,
+                  bene_aged_total   = aged_tot_benes,
+                  bene_aged_esrd    = aged_esrd_benes,
+                  bene_aged_no_esrd = aged_no_esrd_benes,
+                  bene_dsb_total    = dsbld_tot_benes,
+                  bene_dsb_esrd     = dsbld_esrd_and_esrd_only_benes,
+                  bene_dsb_no_esrd  = dsbld_no_esrd_benes,
+                  bene_ab_total     = a_b_tot_benes,
+                  bene_ab_orig      = a_b_orgnl_mdcr_benes,
+                  bene_ab_ma_oth    = a_b_ma_and_oth_benes,
+                  bene_rx_total     = prscrptn_drug_tot_benes,
+                  bene_rx_pdp       = prscrptn_drug_pdp_benes,
+                  bene_rx_mapd      = prscrptn_drug_mapd_benes,
+                  bene_rx_elig      = prscrptn_drug_deemed_eligible_full_lis_benes,
+                  bene_rx_full      = prscrptn_drug_full_lis_benes,
+                  bene_rx_part      = prscrptn_drug_partial_lis_benes,
+                  bene_rx_none      = prscrptn_drug_no_lis_benes) |>
     dplyr::filter(state != "UK") |>
     dplyr::filter(county != "Unknown") |>
     dplyr::mutate(dplyr::across(dplyr::where(is.character), ~dplyr::na_if(., "")),
@@ -164,5 +170,9 @@ beneficiary_enrollment <- function(year        = NULL,
                   dplyr::across(dplyr::contains("bene"), as.integer))
 
   }
+    if (period == "Monthly") {
+      results <- results |>
+        dplyr::filter(period %in% month.name)
+    }
   return(results)
 }
